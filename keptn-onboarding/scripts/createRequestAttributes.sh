8c2dae2675865d22f0e28f510ebbb410b6647c4e
Jeff Yarbrough <pcjeffmac@gmail.com>
2021-03-22 12:28:23 -0400

    Update createRequestAttributes.sh

--- keptn-onboarding/scripts/createRequestAttributes.sh
+++ keptn-onboarding/scripts/createRequestAttributes.sh
@@ -48,7 +48,7 @@ function createRequestAttribute() {
         }
       },
       "parameterName": "x-dynatrace-test",
-      "capturingAndStorageLocation": "CAPTURE_AND_STORE_ON_CLIENT"
+      "capturingAndStorageLocation": "CAPTURE_AND_STORE_ON_SERVER"
     }
   ],
   "normalization": "ORIGINAL",
@@ -80,11 +80,16 @@ function createRequestAttribute() {
 createRequestAttribute "TSN"
 
 ###########################################################################
-# First we create LSN
+# Second we create LSN
 ###########################################################################
 createRequestAttribute "LSN"
 
 ###########################################################################
-# First we create LTN
+# Third we create LTN
 ###########################################################################
 createRequestAttribute "LTN"
+
+###########################################################################
+# Fourth we create LTN
+###########################################################################
+createRequestAttribute "SI"
