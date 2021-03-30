da780f0ccd71818103159bcfbf11a9d432858149
Jeff Yarbrough <pcjeffmac@gmail.com>
2021-03-30 17:00:28 -0400

    Create loadproject.sh

---
+++ easytravel-onboarding/loadproject.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x
+
+~/keptn-in-a-box/resources/easytravel/onboard_easytravel.sh
+
+~/keptn-in-a-box/resources/easytravel/onboard_easytravel_qualitygates.sh
+
+keptn trigger delivery --project=easytravel --service=easytravel-mongodb --image=docker.io/dynatrace/easytravel-mongodb --tag=latest --labels=creator=cli
+sleep 30
+keptn trigger delivery --project=easytravel --service=easytravel-backend --image=docker.io/dynatrace/easytravel-backend --tag=latest --labels=creator=cli
+sleep 30
+keptn trigger delivery --project=easytravel --service=easytravel-frontend --image=docker.io/dynatrace/easytravel-frontend --tag=latest --labels=creator=cli
+sleep 30
+keptn trigger delivery --project=easytravel --service=easytravel-www --image=docker.io/dynatrace/easytravel-nginx --tag=latest --labels=creator=cli
