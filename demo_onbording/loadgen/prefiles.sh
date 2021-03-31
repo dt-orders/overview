b362963904b3975e91b000db37b7766cb68b9334
Jeff Yarbrough <jeff.yarbrough@dynatrace.com>
2021-03-31 09:23:20 -0400

    Update prefiles.sh

--- demo_onbording/loadgen/prefiles.sh
+++ demo_onbording/loadgen/prefiles.sh
@@ -5,3 +5,7 @@ cp ~/examples/load-generation/cartsloadgen/deploy/cartsloadgen-* ~/overview/demo
 DOMAIN=$KEPTN_DOMAIN
 
 echo "changing to domain ${DOMAIN}"
+
+SVC-DOMAIN="svc.cluster.local"
+
+find ./ -exec sed -i 's/${SVC-DOMAIN}/${DOMAIN}/g' {} \;
